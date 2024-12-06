1ea49b55d17049a3dcd5662d36c716f41436395d
rutvik-at-pieces <rutvik@pieces.app>
2024-12-07 00:00:00 +0530

    updates

--- shaders/stripes.glsl
+++ shaders/stripes.glsl
@@ -10,38 +10,32 @@ uniform float warpTiling;
 uniform vec3 color1;
 uniform vec3 color2;
 
-vec2 iResolution=vec2(width,height);
+vec2 iResolution = vec2(width, height);
 
 out vec4 fragColor;
 
-vec2 rotatePoint(vec2 pt,vec2 center,float angle){
-    float sinAngle=sin(angle);
-    float cosAngle=cos(angle);
-    pt-=center;
-    vec2 r=vec2(1.);
-    r.x=pt.x*cosAngle-pt.y*sinAngle;
-    r.y=pt.x*sinAngle+pt.y*cosAngle;
-    r+=center;
+vec2 rotatePoint(vec2 pt, vec2 center, float angle) {
+    float sinAngle = sin(angle);
+    float cosAngle = cos(angle);
+    pt -= center;
+    vec2 r = vec2(1.);
+    r.x = pt.x * cosAngle - pt.y * sinAngle;
+    r.y = pt.x * sinAngle + pt.y * cosAngle;
+    r += center;
     return r;
 }
 
-void main(){
-    vec2 uv=gl_FragCoord.xy/iResolution.xy;
-    
-    // float tiles=tiles;
-    
-    // vec2 uv2=uv;
-    
-    // uv2.x=mix(uv.x,uv.y,direction);
-    // uv2.y=mix(uv.y,1.-uv.x,direction);
-    vec2 uv2=rotatePoint(uv.xy,vec2(.5,.5),direction*2.*PI);
-    
-    uv2.x+=sin(uv2.y*warpTiling*PI*2.)*warpScale+speed;//u_time/10.;
-    uv2.x*=tiles;
-    
-    float st=floor(fract(uv2.x)+.5);
-    
-    vec3 color=mix(color1,color2,st);
-    
-    fragColor=vec4(color,1.);
+void main() {
+    vec2 uv = gl_FragCoord.xy / iResolution.xy;
+
+    vec2 uv2 = rotatePoint(uv.xy, vec2(.5, .5), direction * 2. * PI);
+
+    uv2.x += sin(uv2.y * warpTiling * PI * 2.) * warpScale + speed;
+    uv2.x *= tiles;
+
+    float st = floor(fract(uv2.x) + .5);
+
+    vec3 color = mix(color1, color2, st);
+
+    fragColor = vec4(color, 1.);
 }
